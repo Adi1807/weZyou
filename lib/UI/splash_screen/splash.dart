df7d23782d9ef7a5963dafab15e62bbd131c3916
adi <finalsemprojekt@gmail.com>
2021-11-30 22:58:11 +0530

    splash screen changes

--- lib/UI/splash_screen/splash.dart
+++ lib/UI/splash_screen/splash.dart
@@ -5,9 +5,26 @@ class Splash extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Center(
-        child: Text('hello'),
+    return Material(
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text('Hello'),
+          Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+                onPressed: () {},
+                child: const Text('Get Started'),
+                style: ElevatedButton.styleFrom(
+                    primary: Colors.pink[400],
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 50, vertical: 20),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(30))))),
+          ),
+        ],
       ),
     );
   }
